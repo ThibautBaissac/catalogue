2a9f711b1cabb7d4d0fb555127628b43f6f0308c
Thibaut Baissac <tbaissac@proton.me>
2025-08-03 11:50:39 +0200

    feat: Add autoprefixer and postcss support; update tailwind config for improved content matching

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/renderer/**/*.{html,tsx}'],
+  content: [
+    './src/renderer/**/*.{html,js,ts,jsx,tsx}',
+    './src/renderer/index.html'
+  ],
   darkMode: 'class',
   theme: {
     extend: {
